907cabdb03fb92685f07935ad7b649413e2fa146
Behsa Rasouli <rasoolibehsa@gmail.com>
2025-11-26 16:49:53 +0400

    first commit

---
+++ loginApp/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace loginApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            errorProvider1 = new ErrorProvider(components);
+            errorProvider2 = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider2).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(171, 62);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(166, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(171, 111);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(166, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(58, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 25);
+            label1.TabIndex = 2;
+            label1.Text = "username:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(58, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 25);
+            label2.TabIndex = 3;
+            label2.Text = "password:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(171, 161);
+            button1.Name = "button1";
+            button1.Size = new Size(166, 55);
+            button1.TabIndex = 4;
+            button1.Text = "login";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            errorProvider2.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkBlue;
+            ClientSize = new Size(449, 320);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "loginApp";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private ErrorProvider errorProvider1;
+        private ErrorProvider errorProvider2;
+    }
+}
---
+++ loginApp/Form1.cs
@@ -0,0 +1,69 @@
+using System.Windows.Forms;
+
+namespace loginApp
+{
+    public partial class Form1 : Form
+    {
+        //made pas and name :
+        private string name = "adminbehsa";
+        private string pas = "12345678";
+        // test
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //form1:
+
+            if (textBox1.Text.Equals(Name) && textBox2.Text.Equals(pas))
+            {
+                MessageBox.Show("login success");
+
+            }
+            else
+            {
+                if (!textBox1.Text.Equals(name))
+                {
+                    errorProvider1.SetError(textBox1, "user name is not correct");
+                }
+
+                if (!textBox2.Text.Equals(pas))
+                {
+                    errorProvider1.SetError(textBox2, "password is not correct ");
+
+                }
+                //form2:
+
+                if (textBox1.Text.Equals(Name) && textBox2.Text.Equals(pas))
+                {
+                    MessageBox.Show("login success");
+                    errorProvider1.Clear();
+                    errorProvider2.Clear();
+                }
+                else
+                {
+                    if (!textBox1.Text.Equals(name))
+                    {
+                        errorProvider1.SetError(textBox1, "user name is not correct");
+                    }
+                    else
+                        errorProvider1.Clear();
+                    if (!textBox2.Text.Equals(pas))
+                    {
+                        errorProvider1.SetError(textBox2, "password is not correct");
+                    }
+                    else
+                        errorProvider2.Clear();
+                }
+            }
+        }
+    }
+}
